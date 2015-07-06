e3c4621f859b02a9178e673a7ff1e88b043761d5
Andre Cruz <andre.cruz@co.sapo.pt>
2015-07-06 17:38:23 +0100

    Check multi-arch dir for library.

--- setup.py
+++ setup.py
@@ -54,6 +54,10 @@ class _M2CryptoBuildExt(build_ext.build_ext):
         
         self.swig_opts = ['-I%s' % i for i in self.include_dirs + \
                           [opensslIncludeDir]]
+        if 'DEB_HOST_MULTIARCH' in os.environ:
+            opensslMultiarchIncludeDir = os.path.join(
+                opensslIncludeDir, os.environ['DEB_HOST_MULTIARCH'])
+            self.swig_opts.append('-I%s' % opensslMultiarchIncludeDir)
         self.swig_opts.append('-includeall')
         #self.swig_opts.append('-D__i386__') # Uncomment for early OpenSSL 0.9.7 versions, or on Fedora Core if build fails
         #self.swig_opts.append('-DOPENSSL_NO_EC') # Try uncommenting if you can't build with EC disabled
